c006024402a5b3a95d6b1b067c78526d0e4414ef
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-09 00:01:45 +0100

    more javascript lessons

--- JavaScript Algorithms and Data Structure/Selecting from Many Options with Switch Statements/index2.js
+++ JavaScript Algorithms and Data Structure/Selecting from Many Options with Switch Statements/index2.js
@@ -1,20 +1,22 @@
-// Adding a Default Option in Switch Statements
-function switchOfStuff(val) {
+// Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
   let answer = "";
   switch (val) {
-    case "a":
-      answer = "apple";
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
       break;
-    case "b":
-      answer = "bird";
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
       break;
-    case "c":
-      answer = "cat";
-      break;
-    default:
-      answer = "stuff";
-      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
   }
   return answer;
 }
-console.log(switchOfStuff("a"));
+console.log(sequentialSizes(5));
