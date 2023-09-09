c747274cd8aaa2fa6393611d3bdaa51835ebe4b4
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-09 23:41:49 +0100

    working with objects

---
+++ JavaScript Algorithms and Data Structure/Build JavaScript Objects/index11.js
@@ -0,0 +1,13 @@
+// Accessing Nested Arrays
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+const secondTree = console.log(myPlants[1].list[1]);
---
+++ JavaScript Algorithms and Data Structure/Build JavaScript Objects/index8.js
@@ -0,0 +1,8 @@
+// Testing Objects for Properties
+function checkObj(obj, checkProp) {
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+}
