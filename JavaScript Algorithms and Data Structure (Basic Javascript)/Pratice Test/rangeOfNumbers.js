d574330fd01fe77a06a63cbb74fc6712f6407cb1
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-12 23:13:25 +0100

    JavaScript Algorithms and Data Structures(Basic Javascript)

---
+++ JavaScript Algorithms and Data Structure (Basic Javascript)/Pratice Test/rangeOfNumbers.js
@@ -0,0 +1,11 @@
+// Use Recursion to Create a Range of Numbers
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
+console.log(rangeOfNumbers(1, 10))
