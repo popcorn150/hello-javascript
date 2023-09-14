a4f4ad1274bc9802172ebf318d07cf3e80e10101
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-14 20:39:24 +0100

    Working with  javascript import and export syntax

---
+++ JavaScript Algorithms and Data Structures (ES6)/Module Script [Javascript with Html]/index1.js
@@ -0,0 +1,4 @@
+// Reuse JavaScript Code Using import
+import { uppercaseString, lowercaseString } from "./index0";
+uppercaseString("hello");
+lowercaseString("WORLD!");
--- JavaScript Algorithms and Data Structures (ES6)/Object Literal Declarations/index2.js
+++ JavaScript Algorithms and Data Structures (ES6)/Object Literal Declarations/index2.js
@@ -1,1 +1,19 @@
 // Use class Syntax to Define a Constructor Function
+
+// Explicit constructor
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+const carrot = new Vegetable("carrot");
+console.log(carrot.name); // Should display 'carrot'
+
+// Implicit constructor
+class Veggies {
+  launch() {
+    console.log("carrot");
+  }
+}
+const veggie = new Veggies();
+veggie.launch();
