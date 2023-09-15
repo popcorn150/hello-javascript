4b779946b19bf05617f289a9e7093f55370a9098
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-15 20:43:46 +0100

    Still on js regular expressions

---
+++ JavaScript Algorithms and Data Structures (Regular Experssions)/Using The Test Method/index7.js
@@ -0,0 +1,5 @@
+// Match Letters of the Alphabet
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi;
+let result = quoteSample.match(alphabetRegex);
+console.log(result.length)
