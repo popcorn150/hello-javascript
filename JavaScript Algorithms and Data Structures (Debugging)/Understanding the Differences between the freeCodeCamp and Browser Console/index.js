1a26f1cb08029c1568394b00a79b76ef4d964ed1
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-19 23:38:17 +0100

    javascript debugging

---
+++ JavaScript Algorithms and Data Structures (Debugging)/Understanding the Differences between the freeCodeCamp and Browser Console/index.js
@@ -0,0 +1,5 @@
+// Understanding the Differences between the freeCodeCamp and Browser Console
+let output =
+  "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
