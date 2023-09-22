7023f5adb15d4f0d71942eb4d74bdd8b73c6098d
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-22 20:51:50 +0100

    added a new feature by generating random colors using the 3 basic color mix

--- Building Projects with JavaScript/Generating Different Background Colors with Button Click/script.js
+++ Building Projects with JavaScript/Generating Different Background Colors with Button Click/script.js
@@ -1,11 +1,15 @@
 var index = 0
 
 function changeColors() {
-    var colors = ['red', 'blue', 'yellow', 'green', 'purple', 'black', 'orange', 'pink']
+    // var colors = ['red', 'blue', 'yellow', 'green', 'purple', 'black', 'orange', 'pink']
+    const red = Math.floor(Math.random() * 255)
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
 
-    document.getElementsByTagName('body')[0].style.background = colors[index++]
+    // document.getElementsByTagName('body')[0].style.background = colors[index++]
+    document.getElementsByTagName('body')[0].style.background = `rgb(${red}, ${green}, ${blue})`
 
-    if(index > colors.length - 1){
-        index = 0
-    }
+    // if(index > colors.length - 1){
+    //     index = 0
+    // }
 }
