75f9fffeab72c5943087e1ba71386b3565b3a4d5
Dark <66648063+popcorn150@users.noreply.github.com>
2023-09-29 21:26:50 +0100

    Completd the sticky notes

--- Building Projects with JavaScript (Responsive)/Sticky Notes/script.js
+++ Building Projects with JavaScript (Responsive)/Sticky Notes/script.js
@@ -1,5 +1,5 @@
-let container2 = document.getElementsByClassName("container2");
-let container3 = document.getElementById("container3");
+let container2 = document.getElementsByClassName("container2")[0];
+let container3 = document.getElementsByClassName("container3")[0];
 let checkIcon = document.getElementById("check-icon");
 let xIcon = document.getElementById("x-icon");
 let i = 0;
@@ -29,9 +29,31 @@ function createNote() {
 
   h1Element.innerHTML = noteText;
 
+  h1Element.setAttribute(
+    "style",
+    "width: 250px; height: 250px; font-size: 26px; padding: 25px; margin-top: 10px; over-flow: hidden; box-shadow: 0px 10px 24px 0 rgba(0,0,0,0.75"
+  );
+
+  h1Element.style.margin = margin();
+  h1Element.style.transform = rotate();
+  h1Element.style.background = color();
+
+  h1Element.addEventListener('dblclick', function(){
+    h1Element.remove()
+  })
+
+  h1Element.addEventListener('mouseenter', function(){
+    h1Element.style.transform = 'scale(1.1)'
+  })
+
+  h1Element.addEventListener('mouseleave', function(){
+    h1Element.style.transform = 'scale(1)'
+  })
+
   divElement.appendChild(h1Element);
 
   container2.insertAdjacentElement("beforeend", divElement);
+  document.getElementById('note-text').value = ''
 }
 
 function margin() {
