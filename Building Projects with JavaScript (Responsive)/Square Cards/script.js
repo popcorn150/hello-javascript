18649c140adb039796f5c46e3d543a3bea0cdfff
Dark <66648063+popcorn150@users.noreply.github.com>
2023-10-03 01:02:40 +0100

    Square Cards

---
+++ Building Projects with JavaScript (Responsive)/Square Cards/script.js
@@ -0,0 +1,33 @@
+document
+  .getElementsByClassName("fa-brands fa-youtube")[0]
+  .addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=k4TbAUAp3KU");
+  });
+document
+  .getElementsByClassName("fa-brands fa-youtube")[1]
+  .addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=SmuPepJolcA");
+  });
+document
+  .getElementsByClassName("fa-brands fa-youtube")[2]
+  .addEventListener("click", function () {
+    window.open(
+      "https://www.youtube.com/watch?v=MIYQR-Ybrn4&list=PLjwm_8O3suyOgDS_Z8AWbbq3zpCmR-WE9"
+    );
+  });
+
+document
+  .getElementsByClassName("article")[0]
+  .addEventListener("click", function () {
+    window.open("https://pobsonline.com/my-celebrity-girlfriend-episode-1/");
+  });
+document
+  .getElementsByClassName("article")[1]
+  .addEventListener("click", function () {
+    window.open("https://github.com/miguelznunez/Flashcards");
+  });
+document
+  .getElementsByClassName("article")[2]
+  .addEventListener("click", function () {
+    window.open("https://github.com/popcorn150");
+  });
