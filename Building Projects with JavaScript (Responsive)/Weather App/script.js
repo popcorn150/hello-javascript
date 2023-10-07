7b6be9e2319235e2ee6a83e839cda1edc36597f9
Dark <66648063+popcorn150@users.noreply.github.com>
2023-10-08 00:10:35 +0100

    weather app still in progress...

--- Building Projects with JavaScript (Responsive)/Weather App/script.js
+++ Building Projects with JavaScript (Responsive)/Weather App/script.js
@@ -1,15 +1,23 @@
-const apiKey = '83467daa0af229d4b60215c8c6f2881e'
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=ikeja'
+const apiKey = "83467daa0af229d4b60215c8c6f2881e";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
-    let data = await response.json()
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.getElementById("search-city");
 
-    console.log(data)
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  let data = await response.json();
 
-    document.querySelector('.city').innerHTML = data.name
-    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '˚c'
-    document.querySelector('.humidity').innerHTML = data.main.humidity + '%'
-    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h'
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "˚c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+  searchBtn.addEventListener("click", function () {
+    console.log("yay!!");
+    checkWeather(searchBox.value);
+  });
 }
-checkWeather()
